458f660e4a03c6f45357d950589bc42e706770e0
Matheus Costa <matheus.design12@gmail.com>
2025-03-16 12:46:20 -0300

    fix: Organize project structure

--- Workshops.Web/Program.cs
+++ Workshops.Web/Program.cs
@@ -1,5 +1,6 @@
-using Asp.Versioning;
 using Workshops.Infrastructure.CrossCutting;
+using Workshops.Infrastructure.Identity;
+using Workshops.Web.CrossCutting;
 using Workshops.Web.CrossCutting.Options;
 using Workshops.Web.Utils;
 
@@ -11,36 +12,16 @@ builder.Services.AddControllers(options =>
 {
     options.Conventions.Add(new RoutePrefixConvention("api"));
 });
+
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddApiVersioningExtension();
 
-builder.Services.AddApiVersioning(options =>
-{
-    options.DefaultApiVersion = new ApiVersion(1,0);
-    options.ReportApiVersions = true;
-    options.AssumeDefaultVersionWhenUnspecified = true;
-    options.ApiVersionReader = ApiVersionReader.Combine(
-        new UrlSegmentApiVersionReader(),
-        new HeaderApiVersionReader("X-Version"));
-    
-}).AddApiExplorer(options =>
-{
-    options.GroupNameFormat = "'v'V";
-    options.SubstituteApiVersionInUrl = true;
-});
-
+builder.Services.ConfigureOptions<ConfigureSwaggerOptions>();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-app.UseSwagger();
-app.UseSwaggerUI(options =>
-{
-    foreach (var groupName in app.DescribeApiVersions().Select(info => info.GroupName))
-    {
-        options.SwaggerEndpoint($"/swagger/{groupName}/swagger.json", groupName.ToUpperInvariant());
-    }
-});
-
+app.UseSwaggerExtension();
 app.UseHttpsRedirection();
 
 app.UseAuthentication();
