a5d854583761a807b0ad368b80be040cb79256e5
Matheus Costa <matheus.design12@gmail.com>
2025-03-16 14:50:12 -0300

    feat(Infra): Add Generic Repository concrete

---
+++ Workshops.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,50 @@
+using Microsoft.EntityFrameworkCore;
+using Workshops.Domain.Common;
+using Workshops.Domain.Interfaces;
+using Workshops.Infrastructure.Data;
+
+namespace Workshops.Infrastructure.Repositories;
+
+public class Repository<TEntity>(AppDbContext context) : IRepository<TEntity>, IAsyncDisposable where TEntity : Entity
+{
+    public async Task<List<TEntity>> GetAllAsync()
+    {
+        return await context.Set<TEntity>().ToListAsync();
+    }
+
+    public async Task<TEntity?> GetByIdAsync(int id)
+    {
+        return await context.Set<TEntity>().FindAsync(id);
+    }
+
+    public IQueryable<TEntity> GetQueryable()
+    {
+        return context.Set<TEntity>();
+    }
+
+    public async Task<TEntity> Insert(TEntity entity)
+    {
+        var entry = await context.Set<TEntity>().AddAsync(entity);
+        return entry.Entity;
+    }
+
+    public void Update(TEntity entity)
+    {
+        context.Set<TEntity>().Update(entity);
+    }
+
+    public void Delete(TEntity entity)
+    {
+        context.Set<TEntity>().Remove(entity);
+    }
+    
+    public async Task SaveChangesAsync()
+    {
+        await context.SaveChangesAsync();
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        await context.DisposeAsync();
+    }
+}
