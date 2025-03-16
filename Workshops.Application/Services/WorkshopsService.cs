3079838a7b0f12d833c776ce19f836ebe8e49e54
Matheus Costa <matheus.design12@gmail.com>
2025-03-16 16:02:02 -0300

    feat(Services): Add simple WorkshopsServices

---
+++ Workshops.Application/Services/WorkshopsService.cs
@@ -0,0 +1,111 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Workshops.Application.DTOs.Workshops;
+using Workshops.Application.Utils;
+using Workshops.Domain.Entities;
+using Workshops.Domain.Interfaces;
+
+namespace Workshops.Application.Services;
+
+public interface IWorkshopsService
+{
+}
+
+public class WorkshopsService(IRepository<WorkshopsEntity> workshopsRepository, ILogger<WorkshopsService> logger)
+    : IWorkshopsService
+{
+    public async Task<List<WorkshopsEntity>> GetAllWorkshopsAsync()
+    {
+        return await workshopsRepository.GetAllAsync();
+    }
+
+    /// <summary>
+    /// Retrieves a workshop by its ID.
+    /// </summary>
+    public async Task<WorkshopsEntity?> GetWorkshopByIdAsync(int id)
+    {
+        try
+        {
+            if (id <= 0)
+                throw new ArgumentException("Invalid workshop ID.");
+
+            var workshop = await workshopsRepository.GetByIdAsync(id);
+            if (workshop == null) logger.LogWarning("Workshop with ID {Id} not found.", id);
+
+            return workshop;
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Error fetching workshop with ID {Id}.", id);
+            throw;
+        }
+    }
+
+    public async Task<WorkshopsEntity> InsertWorkshopAsync(WorkshopRequestDto workshopDto)
+    {
+        var slug = SlugHelper.GenerateSlug(workshopDto.Title);
+        var uniqueSlug = slug;
+
+        //Adds a counter to the slug if it already exists ( like nonce with RNG )
+        var slugExists = await workshopsRepository.GetQueryable()
+            .AnyAsync(w => w.Slug.Equals(slug, StringComparison.OrdinalIgnoreCase));
+
+        if (slugExists)
+        {
+            
+            logger.LogWarning("Workshop with Slug {Slug} already exists.", slug);
+            
+            var counter = 1;
+            do
+            {
+                uniqueSlug = $"{slug}-{counter}";
+                counter++;
+                slugExists = await workshopsRepository.GetQueryable()
+                    .AnyAsync(w => w.Slug.Equals(slug, StringComparison.OrdinalIgnoreCase));
+            } while (slugExists);
+        }
+
+        var workshop = new WorkshopsEntity
+        {
+            Name = workshopDto.Title,
+            Description = workshopDto.Description,
+            RealizationDate = workshopDto.Date,
+            Capacity = workshopDto.Capacity ?? null,
+            Slug = uniqueSlug,
+        };
+
+        await workshopsRepository.Insert(workshop);
+        await workshopsRepository.SaveChangesAsync();
+
+        return workshop;
+    }
+    
+    public async Task UpdateWorkshopAsync(int id, WorkshopRequestDto workshopDto)
+    {
+        var workshop = await GetWorkshopByIdAsync(id);
+        if (workshop == null)
+            throw new ArgumentException("Workshop not found.");
+
+        //If the title is being updated, the slug must be updated as well, to keep the uniqueness of the slug
+        //But if the title is the same, the slug remains the same. But the slug would not be updated for simplicity
+        workshop.Name = workshopDto.Title;
+        workshop.Description = workshopDto.Description;
+        workshop.RealizationDate = workshopDto.Date;
+        
+        if (workshopDto.Capacity.HasValue)
+            workshop.Capacity = workshopDto.Capacity.Value;
+
+        workshopsRepository.Update(workshop);
+        await workshopsRepository.SaveChangesAsync();
+    }
+    
+    public async Task DeleteWorkshopAsync(int id)
+    {
+        var workshop = await GetWorkshopByIdAsync(id);
+        if (workshop == null)
+            throw new ArgumentException("Workshop not found.");
+
+        workshopsRepository.Delete(workshop);
+        await workshopsRepository.SaveChangesAsync();
+    }
+}
