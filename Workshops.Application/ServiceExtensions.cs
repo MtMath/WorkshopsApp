057181703a2b9032e11f5b69d3ba1a5018ef1ebe
Matheus Costa <matheus.design12@gmail.com>
2025-03-16 16:34:03 -0300

    feat(Application): Add simple CollaboratorService

--- Workshops.Application/ServiceExtensions.cs
+++ Workshops.Application/ServiceExtensions.cs
@@ -7,6 +7,7 @@ public static class ServiceExtensions
 {
     public static IServiceCollection AddApplicationLayer(this IServiceCollection services)
     {
+        services.AddScoped<CollaboratorService>();
         services.AddScoped<WorkshopsService>();
         
         return services;
